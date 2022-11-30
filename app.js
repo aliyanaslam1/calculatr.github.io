6d7cabe49a9abc0b77d556d66a3773079068697f
aliyanaslam1 <118340179+aliyanaslam1@users.noreply.github.com>
2022-11-30 13:41:52 +0500

    webtac=sk

---
+++ app.js
@@ -0,0 +1,12 @@
+var elem = document.getElementById("display");
+function getValue(a){
+//  console.log(a);
+display.value += a;
+}
+
+function calculate(){
+    a.value = eval(a.value);
+}
+function clearAll(){
+    display.value="0";
+}
